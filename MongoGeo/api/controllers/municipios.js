113ff18916d0bb90bf2802c6abdd19c05e322a29
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-03-24 21:16:26 -0300

    Adiciona função para obter município por ID na API

---
+++ MongoGeo/api/controllers/municipios.js
@@ -0,0 +1,17 @@
+import { ObjectId } from 'mongodb';
+
+export const getMunicipiosById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const db = req.app.locals.db;
+        const municipio = await db.collection('municipios').findOne({ _id: id });
+        if (!municipio) {
+            return res.status(404).json({ error: true, message: 'Municipio not found' });
+        }
+        res.status(200).json(municipio);
+    }
+    catch (error) {
+        res.status(500).json({ error: true, message: 'Internal Server Error' });
+        console.error('Error getting municipio by id', error);
+    }
+}
