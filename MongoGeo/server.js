6ce776b7c59a40a57de3d83c6bdf9741db354b87
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-03-24 21:26:51 -0300

    Adiciona rotas da API para municípios no servidor

--- MongoGeo/server.js
+++ MongoGeo/server.js
@@ -1,12 +1,19 @@
 
 import express from 'express';
 import { connectToDatabase } from './config/db.js';
+import municipiosRoutes from './api/routes/municipios.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+
+// public routes
 app.use('/', express.static('public'));
+// api routes
+app.use('/api/municipios', municipiosRoutes);
+// favicon
 app.use('/favicon.ico', express.static('public/images/logo.png'));
+
 connectToDatabase(app)
     .then(() => {
         app.listen(PORT, () => {
