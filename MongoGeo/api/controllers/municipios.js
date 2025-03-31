ed47e6554b9f55e3f9ad43b26b8f381cbf6881dd
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-03-31 19:53:28 -0300

    Adiciona função para criação de município com validação de campos obrigatórios

--- MongoGeo/api/controllers/municipios.js
+++ MongoGeo/api/controllers/municipios.js
@@ -15,3 +15,24 @@ export const getMunicipiosById = async (req, res) => {
         console.error('Error getting municipio by id', error);
     }
 }
+
+export const createMunicipio = async (req, res) => {
+    try {
+        const { codigo_ibge, nome, capital, codigo_uf, local } = req.body;
+        const db = req.app.locals.db;
+        if (!codigo_ibge || !nome || !codigo_uf) {
+            return res.status(400).json({ error: true, message: 'Missing required fields' });
+        }
+        const existeMunicipio = await db.collection('municipios').findOne({ codigo_ibge })
+        if (existeMunicipio) {
+            return res.status(409).json({ error: true, message: 'Municipio already exists with this IBGE code' });
+        }
+        const novoMunicipio = { codigo_ibge, nome, capital, codigo_uf, local }
+
+        const result = db.collection("municipios").insertOne(novoMunicipio)
+        res.status(201).json({ id: result.insertedId, ...novoMunicipio });
+    } catch (error) {
+        res.status(500).json({ error: true, message: 'Internal Server Error' });
+        console.error('Error creating municipio', error);
+    }
+}
