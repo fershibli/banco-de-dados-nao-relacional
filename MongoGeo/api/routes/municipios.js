8a3a245e833c18947fa706795919f847f319f314
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-03-31 19:53:38 -0300

    Adiciona rota para criação de município na API

--- MongoGeo/api/routes/municipios.js
+++ MongoGeo/api/routes/municipios.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getMunicipiosById } from '../controllers/municipios.js';
+import { createMunicipio, getMunicipiosById } from '../controllers/municipios.js';
 
 const router = express.Router();
 
 router.get('/:id', getMunicipiosById);
+
+router.post("/", createMunicipio)
 
 export default router;
