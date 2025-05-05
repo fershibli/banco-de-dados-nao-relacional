ce6fc261cceeaf4133c40fdd1b009d92efcf10a2
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-05-05 20:47:14 -0300

    Adiciona validações para o modelo de usuário, incluindo nome, e-mail, senha e status

--- MongoGeo/api/middlewares/validations.js
+++ MongoGeo/api/middlewares/validations.js
@@ -34,3 +34,44 @@ export const validateMunicipio = [
         .isFloat({ min: -180, max: 180 }).withMessage('Local coordinates[0] must be a float between -180 and 180'),
     validateRequest
 ]
+
+export const validateUsuario = [
+    check('nome')
+        .not().isEmpty().trim().withMessage('É obrigatório informar o nome')
+        .isAlpha('pt-BR', { ignore: ' ' }).withMessage('Informe apenas texto')
+        .isLength({ min: 3 }).withMessage('Informe no mínimo 3 caracteres')
+        .isLength({ max: 100 }).withMessage('Informe no máximo 100 caracteres'),
+    check('email')
+        .not().isEmpty().trim().withMessage('É obrigatório informar o e-mail')
+        .isEmail().withMessage('Informe um e-mail válido')
+        .isLowercase().withMessage('Não são permitidas letras maiúsculas')
+        .custom((value, { req }) => {
+            return db.collection('usuarios')
+                .find({ email: { $eq: value } }).toArray()
+                .then((email) => {
+                    if (email.length && !req.params.id) {
+                        return Promise.reject(`O e-mail ${value} já existe`)
+                    }
+                })
+        }),
+    check('senha')
+        .not().isEmpty().trim().withMessage('A senha é obrigatória')
+        .isLength({ min: 6 }).withMessage('A senha deve ter no mínimo 6 caracteres')
+        .isStrongPassword({
+            minLength: 6,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1
+        }).withMessage('A senha não é segura. Informe no mínimo 1 caractere maiúsculo, 1 minúsculo, 1 número e 1 caractere especial'),
+    check('ativo')
+        .default(true)
+        .isBoolean().withMessage('O valor deve ser um booleano'),
+    check('tipo')
+        .default('Cliente')
+        .isIn(['Cliente', 'Admin']).withMessage('O tipo deve ser Admin ou Cliente'),
+    check('avatar')
+        .optional({ nullable: true })
+        .isURL().withMessage('A URL do avatar é inválida'),
+    validateRequest
+]   
