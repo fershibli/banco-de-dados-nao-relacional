d47e5e1af84e17f72e963c2ffe25c39a5ee3c9ff
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-05-05 21:35:49 -0300

    Adiciona rota para a API de usuários

--- MongoGeo/api/server.js
+++ MongoGeo/api/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import { connectToDatabase } from '../config/db.js';
 import municipiosRoutes from './routes/municipios.js';
+import usuariosRoutes from './routes/usuarios.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use('/', express.static('public'));
 // api routes
 app.use('/api/municipios', municipiosRoutes);
+app.use('/api/usuarios', usuariosRoutes);
 // favicon
 app.use('/favicon.ico', express.static('public/images/logo.png'));
 
