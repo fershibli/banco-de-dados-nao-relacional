24b9f6aeac17da9fd297bda2599b78a2ca92b1b9
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-05-05 21:47:00 -0300

    Corrige importações para incluir a extensão .js nos arquivos de controle e validação

--- MongoGeo/api/routes/usuarios.js
+++ MongoGeo/api/routes/usuarios.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { insereUsuario } from '../controllers/usuarios';
-import { validateUsuario } from '../middlewares/validations';
+import { insereUsuario } from '../controllers/usuarios.js';
+import { validateUsuario } from '../middlewares/validations.js';
 
 const router = express.Router();
 
