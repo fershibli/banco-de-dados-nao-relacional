af27a5d8585df68d0cd373ec9dab59e8161c8342
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-05-12 19:28:06 -0300

    Adiciona rota para efetuar login no arquivo de rotas de usuários

--- MongoGeo/api/routes/usuarios.js
+++ MongoGeo/api/routes/usuarios.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { insereUsuario } from '../controllers/usuarios.js';
+import { efetuaLogin, insereUsuario } from '../controllers/usuarios.js';
 import { validateUsuario } from '../middlewares/validations.js';
 
 const router = express.Router();
 
 // Rota para inserir um novo usuário
 router.post('/', validateUsuario, insereUsuario);
+
+// Rota para efetuar login
+router.post('/login', efetuaLogin);
 
 export default router;
