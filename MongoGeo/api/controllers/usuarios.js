6a65a44debc8c4f3f45595ea244f507296b136ec
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-05-12 19:26:37 -0300

    Implementa a função de login com validação de usuário e geração de token JWT

--- MongoGeo/api/controllers/usuarios.js
+++ MongoGeo/api/controllers/usuarios.js
@@ -12,3 +12,23 @@ export const insereUsuario = async (req, res) => {
         .then(result => res.status(201).send(result))
         .catch(err => res.status(400).json(err))
 }
+
+export const efetuaLogin = async (req, res) => {
+    const { email, senha } = req.body
+    const db = req.app.locals.db
+    const usuario = await db.collection('usuarios')
+        .findOne({ email })
+        .catch(err => res.status(400).json(err))
+
+    if (!usuario) {
+        return res.status(401).json({ message: 'Dados de login inválidos' })
+    }
+
+    const senhaValida = await bcrypt.compare(senha, usuario.senha)
+    if (!senhaValida) {
+        return res.status(401).json({ message: 'Dados de login inválidos' })
+    }
+
+    const token = jwt.sign({ id: usuario._id }, process.env.JWT_SECRET, { expiresIn: '1h' })
+    res.status(200).json({ token })
+}
