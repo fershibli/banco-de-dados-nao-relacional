607d6a5775d1ce00d8d4625f32c18d66865d938d
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-05-12 22:02:31 -0300

    Adiciona middleware de autenticação na rota de obtenção de municípios

--- MongoGeo/api/routes/municipios.js
+++ MongoGeo/api/routes/municipios.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createMunicipio, getMunicipiosById } from '../controllers/municipios.js';
 import { validateMunicipio } from '../middlewares/validations.js';
+import auth from '../middlewares/autth.js';
 
 const router = express.Router();
 
-router.get('/:id', getMunicipiosById);
+router.get('/:id', auth, getMunicipiosById);
 
 router.post("/", validateMunicipio, createMunicipio)
 
