14be844f211a867e6620fb6f69b503ae9941c9cd
Fernando Luiz Neme Chibli <fernandoluiz.1991@gmail.com>
2025-06-02 20:31:01 -0300

    feat: adiciona testes para a API REST de Municípios sem autenticação

---
+++ MongoGeo/__tests__/testes_municipios.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const baseURL = process.env.BASE_URL || 'http://localhost:3000/api';
+
+describe('API REST de Municipios sem o Token', () => {
+    it('GET / Lista todos os municipios sem token', async () => {
+        const response = await request(baseURL)
+            .get('/municipios')
+            .set('Content-Type', 'application/json')
+            .expect(401);
+    })
+    
+    it('GET - Listar o municipio pelo ID sem token', async () => {
+        const id = "67cf88945d4f674a9a8c3390";
+        const response = await request(baseURL)
+            .get(`/municipios/${id}`)
+            .set('content-type', 'application/json')
+            .expect(401);
+    })
+})
